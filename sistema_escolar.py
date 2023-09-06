fc6fe7f087a45cfe3000030aedae729a5aa0ffaf
Calixto <sims0@live.com.mx>
2023-09-06 08:21:12 -0600

    avances_uno

--- sistema_escolar.py
+++ sistema_escolar.py
@@ -57,29 +57,91 @@ class Alumno:
         return self.cursor.fetchall()
         
     def ver_alumno(self, id):
-        pass
+        self.cursor.execute("""
+        SELECT * FROM maestros                    
+                            """)
+        self.conexion.commit()
+        return self.cursor.fetcone(id)
         
-    def borrar_alumno(self,id):
-        pass
-    
+    def borrar_alumno(self,id,nuevo_nombre,nuevo_apellido,nueva_fecha_nacimiento,nueva_fecha_ingreso,):
+        self.cursor.execute("DELETE FROM alumnos WHERE id = ?", (id,))
+        self.conexion.commit()
     def actualizar_alumno(self,id):
         pass
     def cerrar_conexion(self):
         self.conexion.close()        
-
-
-
         
+class Mestro:
+    def __init__(self,nombre,apellido,año_de_nacimiento,fecha_de_ingreso) -> None:
+        
+        self.nombre = nombre
+        self.apellido = apellido
+        self.año_de_nacimiento = año_de_nacimiento
+        self.fecha_de_ingreso = fecha_de_ingreso
+        
+    def __str__(self) -> str:
+        return (f"{self.nombre} {self.apellido} {self.año_de_nacimiento}")
+
+# creamos el crud para la base de datos 
+    def iniciar_conexion(self, db_maestros) -> None:
+        self.conexion = sqlite3.connect(db_maestros)
+        self.cursor = self.conexion.cursor()
+        
+    def crear_tablas(self):    
+        self.cursor.execute ("""
+            CREATE TABLE IF NOT EXISTS maestros (
+                id INTEGER PRIMARY KEY,
+                nombre TEXT,
+                apellido TEXT,
+                año_de_nacimiento INTEGER ,
+                fecha_de_ingreso TEXT   
+                    )
+               """)
+        self.conexion.commit()
+        
+        
+    def crear_alumno(self):
+        self.cursor.execute(""" 
+            INSERT INTO maestros (nombre, apellido, año_de_nacimiento,
+                fecha_de_ingreso)
+                VALUES (?,?,?,?,?)""",
+                (self.nombre, self.apellido, self.año_de_nacimiento,self.fecha_de_ingreso))
+        self.conexion.commit()
+    
+    def ver_maestros(self):
+        self.cursor.execute("""
+        SELECT * FROM maestros                    
+                            """)
+        self.conexion.commit()
+        return self.cursor.fetchall()
+        
+    def ver_maestro(self, id):
+        self.cursor.execute("""
+        SELECT * FROM maestros                    
+                            """)
+        self.conexion.commit()
+        return self.cursor.fetchone(id)
+        
+    def borrar_maestro(self,id):
+        self.cursor.execute("DELETE FROM mestros WHERE id = ?", (id,))
+        self.conexion.commit()
+    def actualizar_maestro(self,id,nuevo_nombre,nuevo_apellido,nueva_fecha_nacimiento,):
+        pass
+    def cerrar_conexion(self):
+        self.conexion.close()           
 
 
-# ejecutamos el menu     
+# ejecutamos el menu 
 
-
-menu()
-alumno = Alumno("Jose", " Calixto", 1995, 2020,4)
-#print(alumno)
-alumno.iniciar_conexion("alumnos.db")
-alumno.crear_tablas()
-#alumno.crear_alumno()
-print(alumno.ver_alumnos())
+while(True):
+    menu()
+    opcion = int(input("Ingresa Una Opción."))
+    if opcion == 1:pass
+    elif opcion == 2:pass
+    elif opcion == 3: pass
+    elif opcion == 4: pass
+    elif opcion == 5: exit()
+    else: print("Opcion Invalida Reintente Por favor.")
+        
+    
 
